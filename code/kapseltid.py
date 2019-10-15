2334973ccbb8158659e5062a331a2cb9b8196e53
Henrik Gjestang <henrik.logj@gmail.com>
2019-10-15 20:29:56 +0200

    added print in main and added 00 in beginning of timecodes

--- code/kapseltid.py
+++ code/kapseltid.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 # Hvis du kjører 'pip install timecode" først
-# Så kan du kjøre "python kapseltid.py 00:00:00:28 00:00:32:24" etterpå for å finne riktig tidskode i taggeverktøyet
+# Example "python kapseltid.py 00:00:00:28 00:00:32:24" etterpå for å finne riktig tidskode i taggeverktøyet
 
 from timecode import *
 import sys
 
 def calc_time(start_time, capsule_time):
+    start_time = '00:'+start_time
+    capsule_time = '00:'+capsule_time
+    
     s1 = Timecode(60, start_time)
     c = Timecode(60, capsule_time)
 
@@ -15,7 +18,7 @@ def calc_time(start_time, capsule_time):
     frames = v.frames*2
     tc = Timecode(30, frames=frames)
 
-#     print("Frames: %d, Timecode: %s" % (frames, tc))
+    # print("Frames: %d, Timecode: %s" % (frames, tc))
     return int(frames)
 
 if __name__ == '__main__':
@@ -24,4 +27,5 @@ if __name__ == '__main__':
         print("start_tid er tid til forste screenshot.")
         print("Eksempel: %s 00:00:00:28 00:00:03:27" % (sys.argv[0]))
         sys.exit(1)
-    calc_time(sys.argv[1], sys.argv[2])
+        
+    print (calc_time(sys.argv[1], sys.argv[2]))
