409eee54a60f09d6c8a7915117bf38a4e3396e68
Henrik Gjestang <henrik.logj@gmail.com>
2020-02-05 23:49:23 +0100

    still trying to move code over to utils, not yet working

--- code/utils/dataprep.py
+++ code/utils/dataprep.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from glob import glob
 from os.path import join
 
-def create_dataset(data_dir, outcasts=None, binary=False, neg_class=None, Verbose=True):
+def create_dataset(data_dir, outcasts=None, binary=False, neg_class=None, verbose=False):
     """
     Create a tf.data dataset
     
@@ -27,17 +27,23 @@ def create_dataset(data_dir, outcasts=None, binary=False, neg_class=None, Verbos
     
     # For each folder add files
     for folder in directories:
-        files.extend(glob(join(data_dir,folder,'*.png')))
+        files.extend(glob(join(data_dir,folder,'*.jpg')))
+        
     
     DS_SIZE = len(files)
     
     if binary:
-#         assert neg_class!=None, "Must give a negative class"
+        assert neg_class!=None, "Must give a negative class"
         
-#         pos_class = directories
-#         pos_class.remove(neg_class)
-        return files
+        pos_class = directories
+        pos_class.remove(neg_class)
+        if verbose:
+            print ("Positive class names: ", pos_class)
+            print ("Negative class names: ", neg_class)
+            print ()
     
     else: 
-        return files
+        continue()
     
+    return_dict = {'ds': tf.data.Dataset.list_files(files),
+                   'ds_size': DS_SIZE}
