e11e485fbf6f65ee74af5b93a2e2558336e66e8e
Henrik <henrik.logj@gmail.com>
2020-05-02 17:07:27 +0200

    moved class_distribution over to utils

--- code/utils/data_prep.py
+++ code/utils/data_prep.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-
 import numpy as np
 import os
 import pathlib
 import matplotlib.pyplot as plt
 import scipy.ndimage as ndimage
+
+from utils import class_distribution
 
 def create_dataset(conf):
     """
@@ -342,38 +343,6 @@ def calculate_weights(count_ds, num_classes):
     return score
 
 
-
-
-def class_distribution(count_ds, num_classes, count_batches=10, bs=1024):
-    """
-    Find distribution of dataset by counting a subset.
-    Args: count_ds - dataset to be counted.
-    Return: a list of class distributions
-    """
-    def count(counts, batch):
-        _, labels = batch
-        for i in range(num_classes):
-            counts['class_{}'.format(i)] += tf.reduce_sum(tf.cast(labels == i, tf.int32))
-        return counts
-    
-    # Batch dataset
-    count_ds = count_ds.batch(bs)
-    # Set the initial states to zero
-    initial_state = {}
-    for i in range(num_classes):
-        initial_state['class_{}'.format(i)] = 0
-        
-    counts = count_ds.take(count_batches).reduce(
-                initial_state = initial_state,
-                reduce_func = count)
-
-    final_counts = []
-    for class_, value in counts.items():
-                final_counts.append(value.numpy().astype(np.float32))
-
-    final_counts = np.asarray(final_counts)
-    distribution = final_counts/final_counts.sum()
-    return distribution
 
 
 def resample(ds, num_classes, conf):
