389e14abe494144fc5e7a32faa221deb826bb4fd
Henrik Gjestang <henrik.logj@gmail.com>
2020-05-12 20:32:18 +0200

    made plot_lr_and_accuracy, and show_dataset_predictions

--- code/utils/model_evaluation.py
+++ code/utils/model_evaluation.py
@@ -123,3 +123,79 @@ def display_confusion_matrix(true_labels, predicted_labels, classes):
         columns=pd.MultiIndex(levels=[['Predicted:'], classes], codes=level_labels), 
         )
     print(cm_frame)
+    
+
+
+def plot_lr_and_accuracy(history, conf):
+    """
+    """
+    acc = history.history['sparse_categorical_accuracy']
+    val_acc = history.history['val_sparse_categorical_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    if conf["learning_schedule"]: 
+        lr = history.history['lr']
+    
+    epochs_range = range(history.epoch[-1]+1)
+    
+    if conf["learning_schedule"]:
+        # Plot the learning rate
+        plt.figure(figsize=(8, 6))
+        plt.plot(epochs_range, lr, label='Learning Rate')
+        plt.xlabel('Epoch')
+        plt.ylabel('Learnign rate')
+        plt.title('Learning Rate development during training');
+        plt.savefig(conf["log_dir"]+'/learning_rate.pdf', format='pdf')
+    
+    
+    # Plot train-val accuracy and loss
+    plt.figure(figsize=(14, 6))
+
+    # Subplot 1
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    # plt.ylim([0.5, 1])
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training and Validation Accuracy')
+
+    # Subplot 2
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylim([0.0, 3])
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training and Validation Loss')
+    plt.savefig(conf["log_dir"]+'/accuracy_and_loss.pdf', format='pdf')
+    plt.show()
+    
+
+
+def show_dataset_predictions(true_labels, pred_labels, pred_confidence, images, conf, params):
+    grid_width = 5
+    grid_height = 5
+    f, ax = plt.subplots(grid_width, grid_height)
+    f.set_size_inches(22, 22)
+
+    img_idx = 0
+    for i in range(0, grid_width):
+        for j in range(0, grid_height):
+            actual = params["class_names"][true_labels[img_idx]]
+            lab = params["class_names"][pred_labels[img_idx]]
+            pred = np.round(pred_labels[img_idx], 2)
+
+            title = 'Actual: '+actual+'\nPred: '+lab+ '\nConf: '+str(pred)
+            ax[i][j].axis('off')
+            ax[i][j].set_title(title)
+            ax[i][j].imshow(images[img_idx])
+            img_idx += 1
+
+    plt.subplots_adjust(left=0, bottom=0, right=2, top=2, wspace=0.5, hspace=0.5)
+    plt.tight_layout()
+    plt.savefig("{}/evaluation_predictions.pdf".format(conf["log_dir"]), format="pdf")
