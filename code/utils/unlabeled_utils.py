99c3d2bdb15bf844a0b5a745a8e33882ade672fb
Henrik Gjestang <henrik.logj@gmail.com>
2020-06-16 20:19:26 +0200

    removed duplicate funciton, changed naming convention from unlab to pseudo, moved checkout_findings and checkout_class from utils

--- code/utils/unlabeled_utils.py
+++ code/utils/unlabeled_utils.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 import time
+import textwrap
 
 from tqdm.notebook import tqdm
 from IPython.display import clear_output
+from PIL import Image, ImageDraw, ImageFont
+from utils import print_bar_chart, write_to_file, fn2img, tf_bincount
 
-from utils import print_bar_chart, write_to_file, fn2img
 
 
-
-def generate_labels(count, unlab, unlab_ds, unlab_size, model, conf):
+def generate_labels(count, pseudo, unlab_ds, unlab_size, model, conf):
     """
     Generate new psuedo labels from unlabeled dataset
     """
@@ -43,16 +45,18 @@ def generate_labels(count, unlab, unlab_ds, unlab_size, model, conf):
             if highest_pred > conf["keep_threshold"]:
                 pred_idx = np.argmax(pred).astype(np.uint8)
 
-                unlab["lab_list"].append(pred_idx)
-                unlab["pred_list"].append(highest_pred)
-                unlab["name_list"].append(path)
+                pseudo["lab_list"].append(pred_idx)
+                pseudo["pred_list"].append(highest_pred)
+                pseudo["name_list"].append(path)
 
                 # Clear old bar chart, generate new one and refresh the tqdm progress bars
                 # NB, tqdm run-timer is also reset, unfortunately
                 if not count["findings"]%500 and count["findings"]>100:
                     clear_output(wait=True)
                     tqdm_predicting, tqdm_findings = get_tqdm(unlab_size, count["findings"], tot_cnt)
-                    plot_and_save(unlab["lab_list"])
+                    plot_and_save(pseudo["lab_list"])
+                    with open(conf["log_dir"]+"/unlab_findings.pkl", 'wb') as f:
+                        pickle.dump(pseudo, f)
                     
                 count["findings"] += 1   # previously findings_cnt
                 tqdm_findings.update(1)
@@ -62,13 +66,15 @@ def generate_labels(count, unlab, unlab_ds, unlab_size, model, conf):
         count["total"] = tot_cnt
         print ("Exiting")
         tqdm_predicting, tqdm_findings = get_tqdm(unlab_size, count["findings"], count["total"])
-        plot_and_save(unlab["lab_list"])
+        plot_and_save(pseudo["lab_list"])
+        with open(conf["log_dir"]+"/unlab_findings.pkl", 'wb') as f:
+            pickle.dump(pseudo, f)
 
     finally:
         print ("\nTotal run time: {:.1f} min.".format( (time.time() - total_time)/60 ))
         print ("Found {} new samples in unlabeled_ds after looking at {} images.".format(count["findings"], tot_cnt))
         
-    return unlab, count
+    return pseudo, count
 
 
 
@@ -84,14 +90,14 @@ def get_tqdm(unlab_size, findings_cnt, tot_cnt):
 
 
 
-def custom_sort(unlab_findings):
+def custom_sort(pseudo):
     """
     Takes three lists and return three sorted list based on prediction confidence
     """
     # Unpack
-    pred = unlab_findings["pred_list"]
-    lab = unlab_findings["lab_list"]
-    name = unlab_findings["name_list"]
+    pred = pseudo["pred_list"]
+    lab = pseudo["lab_list"]
+    name = pseudo["name_list"]
     
     # Sort
     sorted_list = list(zip(pred, lab, name))
@@ -102,17 +108,17 @@ def custom_sort(unlab_findings):
     name_sorted = [row[2] for row in sorted_list]
     
     # Re-pack
-    unlab_findings = {
+    pseudo = {
         "pred_list": pred_sorted,
         "lab_list": lab_sorted,
         "name_list": name_sorted
     }
     
-    return unlab_findings
+    return pseudo
 
 
 
-def resample_unlab(unlab, orig_dist,  conf):
+def resample_unlab(pseudo, orig_dist,  conf):
     """
     Resample unlabeled dataset based upon a given distribution.
     """
@@ -120,12 +126,13 @@ def resample_unlab(unlab, orig_dist,  conf):
     
     num_to_match = np.max(orig_dist)
     idx_to_match = np.argmax(orig_dist)
-    print ('Limit set by {} with {} samples'.format(conf["class_names"][idx_to_match], int(num_to_match)))
-    print ("-"*40)
+    if conf["verbosity"]:
+        print ('Limit set by {} with {} samples'.format(conf["class_names"][idx_to_match], int(num_to_match)))
+        print ("-"*40)
 
     new_findings = ([], [])
     new_findings_filepaths = []
-    lab_arr = np.asarray(unlab["lab_list"], dtype=np.uint8)
+    lab_arr = np.asarray(pseudo["lab_list"], dtype=np.uint8)
 
     for class_idx in range(conf["num_classes"]):
         # how many samples already in this class
@@ -139,65 +146,26 @@ def resample_unlab(unlab, orig_dist,  conf):
             if in_count >= num_to_match:
                 count -= 1 # reduce by one cuz of enumerate updates index early
                 break
-            fn = unlab["name_list"][idx]
+            fn = pseudo["name_list"][idx]
             img = fn2img(fn, conf["unlab_dir"], conf["img_shape"][0])
             
             new_findings[0].append(img)                            # image
-            new_findings[1].append(unlab["lab_list"][idx])         # label
-            new_findings_filepaths.append(unlab["name_list"][idx]) # filepath
+            new_findings[1].append(pseudo["lab_list"][idx])         # label
+            new_findings_filepaths.append(pseudo["name_list"][idx]) # filepath
             in_count += 1
         
         if conf["verbosity"]:
             print ("{:27}: added {}/{} samples.".format(conf["class_names"][class_idx], count, num_new_findings))
-            added[conf["class_names"][class_idx]] = [count, num_new_findings]
             
+        added[conf["class_names"][class_idx]] = [count, num_new_findings]
     write_to_file(added, conf, 'samples_added_to_train')
         
     return new_findings, new_findings_filepaths
 
 
 
-def resample_unlab_orig(unlab, orig_dist,  conf):
-    """
-    Resample unlabeled dataset based upon a given distribution.
-    """
-    num_to_match = np.max(orig_dist)
-    idx_to_match = np.argmax(orig_dist)
-    print ('Limit set by {} with {} samples'.format(conf["class_names"][idx_to_match], int(num_to_match)))
-    print ("-"*40)
 
-    new_findings = ([], [])
-    new_findings_filepaths = []
-    lab_arr = np.asarray(unlab["lab_list"], dtype=np.uint8)
-
-    for class_idx in range(conf["num_classes"]):
-        # how many samples already in this class
-        in_count = orig_dist[class_idx]
-
-        indexes = np.where(lab_arr==class_idx)[0]
-        num_new_findings = len(indexes)
-
-        count = 0
-        for count, idx in enumerate(indexes, start=1):
-            if in_count >= num_to_match:
-                count -= 1 # reduce by one cuz of enumerate updates index early
-                break
-            fn = unlab["name_list"][idx]
-            img = fn2img(fn, conf["unlab_dir"], conf["img_shape"][0])
-            
-            new_findings[0].append(img)         # image
-            new_findings[1].append(unlab["lab_list"][idx])         # label
-            new_findings_filepaths.append(unlab["name_list"][idx]) # filepath
-            in_count += 1
-        
-        if conf["verbosity"]:
-            print ("{:27}: added {}/{} samples.".format(conf["class_names"][class_idx], count, num_new_findings))
-    
-    return new_findings, new_findings_filepaths
-
-
-
-def reduce_dataset(ds, remove=None):
+def reduce_dataset(ds, unlab_size, remove=None):
     """
     Remove samples which are combined into the training data from the unlabeled data
     """
@@ -210,4 +178,194 @@ def reduce_dataset(ds, remove=None):
         in_list = tf.math.count_nonzero(bool_list) > 0
         return not in_list
     
-    return ds.filter(remove_samples)
+    new_unlab_ds = ds.filter(remove_samples)
+    new_unlab_size = unlab_size - len(remove)
+    return new_unlab_ds, new_unlab_size
+
+
+
+
+def resample_and_combine(ds, conf, pseudo, pseudo_sorted, datasets_bin):
+    """
+    """
+    if conf["resample"]:
+        new_findings, added_samples = resample_unlab(pseudo_sorted, datasets_bin[-1], conf)
+        # create tf.tensor of the new findings
+        findings_tensor = tf.data.Dataset.from_tensor_slices(new_findings)
+    else:
+        added_samples = pseudo["name_list"]
+        img_list = [fn2img(name, conf["unlab_dir"], conf["img_shape"][0]) for name in pseudo["name_list"]]
+        findings_tensor = tf.data.Dataset.from_tensor_slices((img_list, pseudo["lab_list"]))
+
+    # combine with original training_ds (using clean_ds which is not augmented/repeated etc)
+    if len(added_samples) != 0: # if no samples are added just re-use previous combined_train
+        ds["combined_train"] = ds["combined_train"].concatenate(findings_tensor)
+
+    # count samples in the new/combined dataset
+    datasets_bin.append(tf_bincount(ds["combined_train"], conf["num_classes"]))
+
+    # History of class distribution
+    print_bar_chart(
+        data=datasets_bin,
+        conf=conf,
+        title=None,
+        fname="bar_chart-distribution"
+    )
+    
+    return datasets_bin, added_samples
+
+
+
+def update_sanity(sanity, added_count, unlab_size, datasets_bin, conf):
+    """
+    """
+    sanity = [{"added_samples": added_count,
+               "last_unlab_size": unlab_size,
+               "curr_unlab_size": unlab_size - added_count,
+               "last_train_size": int(np.sum(datasets_bin[-2])),
+               "curr_train_size": int(np.sum(datasets_bin[-1]))
+              }]
+    write_to_file(sanity, conf, "sanity")
+    
+    return sanity
+
+
+
+
+def checkout_findings(pseudo, conf):
+    """
+    Create a large plot of 6 samples from every class found in the unlabeled dataset
+    """
+    ### Create images with label names
+    class_label_img = []
+    font_path = '/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf'
+    img_width = 512
+    font_size = int(img_width*0.15)
+    letters_per_line = 13
+
+    for i in range(conf["num_classes"]):
+        img = Image.new('RGB', (img_width, img_width), color = (0, 0, 0))
+        fnt = ImageFont.truetype(font_path, font_size)
+        d = ImageDraw.Draw(img)
+        if (len(conf["class_names"][i])>letters_per_line):
+            text = textwrap.fill(conf["class_names"][i], width=letters_per_line)
+        else:
+            text = conf["class_names"][i]
+        linebreaks = text.count('\n')
+        d.text((1,(img_width//2.2)-linebreaks*img_width*0.1), text, font=fnt, fill=(255, 255, 255))
+
+        class_label_img.append(img)
+        
+    ### Create a list with 6 samples per class
+    # black image
+    img_black = Image.new('RGB', (conf["img_shape"][0], conf["img_shape"][1]), color = (0, 0, 0))
+    
+    lab_arr = np.asarray(pseudo["lab_list"], dtype=np.uint8)
+    class_examples = []
+    class_preds = []
+    for class_idx in range(conf["num_classes"]):
+        curr_class_examples = []
+        curr_class_preds = []
+
+        indekser = np.where(lab_arr==class_idx)[0]
+        for i in range(6):
+            # get 6 finding images from class_idx-class
+            # no image - index out of bounds
+            if not indekser.size > i:
+                curr_class_examples.append(img_black)
+                curr_class_preds.append(0)
+            # found image
+            else:
+                fn = pseudo["name_list"][indekser[i]]
+                img = fn2img(fn, conf["unlab_dir"], img_width)
+                
+                curr_class_examples.append(img)
+                curr_class_preds.append(pseudo["pred_list"][indekser[i]])
+
+        class_examples.append(curr_class_examples)
+        class_preds.append(curr_class_preds)
+
+    assert (len(conf["class_names"])==len(class_examples)), 'must be same length'
+    
+    ### Display the predicted images in each class
+    # settings
+    nrows, ncols = conf["num_classes"], 7  # array of sub-plots
+    figsize = [ncols*3, conf["num_classes"]*3]     # figure size, inches
+
+    # create figure (fig), and array of axes (ax)
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, 
+                           figsize=figsize, frameon=False, facecolor='white')
+
+    # plot simple raster image on each sub-plot
+    for i, axi in enumerate(ax.flat):
+        # i runs from 0 to (nrows*ncols-1)
+        # axi is equivalent with ax[rowid][colid]
+        rowid = i // ncols
+        colid = i % ncols
+
+        if colid == 0:
+            img = class_label_img[rowid]
+        else:
+            pred = class_preds[rowid][colid-1]
+            title = "conf: "+str(round(pred, 3))
+            if pred: axi.set_title(title)
+            img = class_examples[rowid][colid-1]
+            
+        axi.imshow(img)
+        axi.set_axis_off()
+
+    plt.axis('off')
+    plt.tight_layout(True)
+    plt.savefig("{}/checkout-all.pdf".format(conf["log_dir"]), format='pdf')
+    plt.show()
+
+
+
+def checkout_class(checkout, pseudo, conf):
+    """
+    Display a grid with sample images from one specified class
+    """    
+    # Which class number correspond to that class name
+    class_idx = np.where(conf["class_names"] == checkout)[0]
+    if len(class_idx) == 0:
+        raise NameError('Error: class-name not found. Check spelling.')
+        
+    # List of img_list-indexes with images corresponding to that class number
+    idx_list = np.where(pseudo["lab_list"] == class_idx[0])[0]
+    num_images = len(idx_list)
+    if (num_images == 0):
+        raise IndexError("No findings in this class.")
+    
+    # settings
+    figint = np.sqrt(num_images)
+    nrows = int(np.floor(figint)) if figint <= 5 else 5
+    ncols = int(np.ceil(figint)) if figint <= 5 else 5
+    figsize = [ncols*3, nrows*3]     # figure size, inches
+
+    # create figure (fig), and array of axes (ax)
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, 
+                           figsize=figsize, frameon=False, facecolor='white')
+    
+    # plot simple raster image on each sub-plot
+    for i, axi in enumerate(ax.flat):
+        idx = idx_list[i]
+        # i runs from 0 to (nrows*ncols-1)
+        # axi is equivalent with ax[rowid][colid]
+        try:
+            fn = pseudo["name_list"][idx]
+            img = fn2img(fn, conf["unlab_dir"], 256)
+            
+            pred = pseudo["pred_list"][idx_list[i]]
+            title = "conf: "+str(round(pred, 5))
+            axi.set_title(title)
+            axi.imshow(img)
+        except IndexError:
+            # No more images - skip last suplots
+            pass
+        finally:
+            axi.set_axis_off()
+    
+    plt.axis('off')
+    plt.tight_layout(True)
+    plt.savefig("{}/checkout-{}.pdf".format(conf["log_dir"], checkout), format='pdf')
+    plt.show()
