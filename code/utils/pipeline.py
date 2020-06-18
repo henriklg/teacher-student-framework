834fe06b1094c27fd48a45aa08b38495f82f7f35
Henrik Gjestang <henrik.logj@gmail.com>
2020-06-18 13:25:49 +0200

    merged create-datasets funcitons. now handles binary and multiclass

--- code/utils/pipeline.py
+++ code/utils/pipeline.py
@@ -81,7 +81,7 @@ def create_dataset(conf):
     """
     Create a tf.data Dataset based on a config file.
     Expects dataset to be stored in data_dir/split/categories. 
-    Don't currently accept binary ds.
+    Works for both binary class dataset and multiclass dataset.
     
     Pipeline: list_files -> get_label -> read_file -> decode_image -> resample
                 -> prepare_for_training
@@ -90,9 +90,10 @@ def create_dataset(conf):
     conf - a dictionary with configuration settings
     
     Return:
-    3x tf.data.Dataset for each train, test and val
+    3x tf.data.Dataset for each train, test and val in a dictionary
     Dictionary with some settings like ds_size, class-names etc.
     """
+    global POS_CLASS_NAMES
     global CLASS_NAMES
     global IMG_SIZE
     IMG_SIZE = conf["img_shape"][0]
@@ -116,6 +117,27 @@ def create_dataset(conf):
         class_names = np.delete(class_names, np.where(conf["outcast"] == class_names))
         if verbosity > 0: print ("Removed outcast:", conf["outcast"], end="\n\n")
     conf["num_classes"] = len(class_names)
+    conf["class_names"] = class_names
+    
+    ### IF BINARY ###
+    directories = class_names
+    if conf["ds_info"] == 'binary':
+        class_names = np.array(['Negative','Positive'])
+        conf["num_classes"] = 2
+        conf["class_names"] = class_names
+        neg_class_name = conf['neg_class'] # 'normal'-class
+        pos_class_names = np.delete(directories, np.where(neg_class_name == directories))
+        POS_CLASS_NAMES = pos_class_names
+        
+        # Print info about neg/pos split
+        if verbosity:
+            print_bin_class_info(conf, directories, neg_class_name, pos_class_names
+            )
+        decode_img_lab = process_path_bin
+    else:
+        decode_img_lab = process_path
+    ###
+    
     
     # Create a tf.dataset of the file paths
     ds = {split: str(data_dir/split/'*/*.*g') for split in ["train","test","val"]}
@@ -138,7 +160,7 @@ def create_dataset(conf):
     
     # Create dataset of label, image pairs from the tf.dataset of image paths
     for split in ds:
-        ds[split] = ds[split].map(process_path, num_parallel_calls=AUTOTUNE)
+        ds[split] = ds[split].map(decode_img_lab, num_parallel_calls=AUTOTUNE)
     
     # Save a clean copy of training data
     clean_train = ds["train"]
@@ -146,14 +168,13 @@ def create_dataset(conf):
     # print info about the dataset split
     if verbosity:
         cnt_per_class = get_dataset_info(class_names, data_dir, ds_size)
-        print_split_info(ds, conf["num_classes"], class_names, cnt_per_class, ds_sizes)
+        print_split_info(ds, class_names, cnt_per_class, ds_sizes)
     
     # Cache, shuffle, repeat, batch, prefetch pipeline
     for split in ds:
         ds[split] = prepare_for_training(ds[split], split, conf, cache=True)
     
     steps = {name:ds_sizes[name]//conf["batch_size"] for (name, data) in ds.items()}
-    conf["class_names"] = class_names
     conf["ds_sizes"] = ds_sizes
     conf["steps"] = steps
     ds["clean_train"] = clean_train
@@ -223,7 +244,7 @@ def oversample(ds, cache_name, conf):
         print (initial_dist)
 
     ## Prep cache
-    cache_dir = './cache/{}_{}_{}_resampled/'.format(
+    cache_dir = './cache/{}_resampled_{}_{}/'.format(
         conf["img_shape"][0], 
         conf["ds_info"],
         cache_name
@@ -331,229 +352,8 @@ def create_unlab_ds(conf):
     
     unlab_ds = list_ds_unlabeled.map(process_path_unlab, num_parallel_calls=AUTOTUNE)
 
-    print ("Loaded {} images into unlabeled_ds.".format(ds_size_unlab))
+    if conf["verbosity"]:
+        print ("Loaded {} images into unlabeled_ds.".format(ds_size_unlab))
     
     conf["ds_sizes"]["unlab"] = ds_size_unlab
     return unlab_ds
-
-
-
-
-def split_and_create_dataset(conf):
-    """
-    NB: Old method no longer in use, but still here for legacy
-    Create a tf.data Dataset based on a config file.
-    Expects dataset to be stored in data_dir/categories. This also accepts binary datasets.
-    pipeline: list_files -> get_label -> read_file -> decode_image -> split -> resample
-                -> prepare_for_training
-    
-    Args:
-    conf - a dictionary with configuration settings
-    
-    Return:
-    3x tf.data.Dataset for each train, test and val
-    Dictionary with some settings like ds_size, class-names etc.
-    """
-    global POS_CLASS_NAMES
-    global IMG_SIZE
-    IMG_SIZE = conf["img_shape"][0]
-    # Some parameters
-    data_dir = conf["data_dir"]
-    outcast = conf["outcast"]
-    verbosity = conf["verbosity"]
-    shuffle_buffer_size = conf["shuffle_buffer_size"]
-    seed = conf["seed"]
-
-    pathlib.Path(conf["cache_dir"]).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(conf["log_dir"]).mkdir(parents=True, exist_ok=True)
-    np.random.seed(seed=seed)
-    neg_count = pos_count = 0
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    ds_size = len(list(data_dir.glob('*/*.*g')))
-    directories = np.array([item.name for item in data_dir.glob('*') if item.name != 'metadata.json'])
-    
-    # Remove the outcast folder
-    if outcast != None:
-        directories = np.delete(directories, np.where(outcast == directories))
-        if verbosity > 0: print ("Removed outcast:", outcast, end="\n\n")
-
-    ## Print info about the dataset
-    # Binary dataset
-    if conf["ds_info"] == 'binary':
-        class_names = np.array(['Negative','Positive'])
-        num_classes = len(class_names)
-        neg_class_name = conf['neg_class'] # 'normal'-class
-        pos_class_names = np.delete(directories, np.where(neg_class_name == directories))
-        POS_CLASS_NAMES = pos_class_names
-        # Print info about neg/pos split
-        if verbosity > 0: 
-            ds_size, neg_count, pos_count = print_bin_class_info(
-                                                directories, data_dir, 
-                                                ds_size, outcast, class_names, 
-                                                neg_class_name, pos_class_names
-            )
-        conf["neg_count"] = neg_count
-        conf["pos_count"] = pos_count
-    # Full dataset
-    else:     
-        class_names = directories
-        num_classes = len(class_names)
-        # Print info about classes
-        if verbosity > 0: 
-            ds_size = print_class_info(directories, data_dir, ds_size, outcast)
-    
-    # Create a tf.dataset of the file paths
-    if outcast == None:
-        files_string = str(data_dir/'*/*.*g')
-    else:
-        files_string = str(data_dir/'[!{}]*/*'.format(outcast))
-    
-    if verbosity > 0: print ("Dataset.list_files: ",files_string, "\n")
-    
-    
-    list_ds = tf.data.Dataset.list_files(
-            files_string, 
-            shuffle=shuffle_buffer_size>1, 
-            seed=tf.constant(seed, tf.int64) if seed else None
-    )
-    
-
-    # Create dataset of label, image pairs from the tf.dataset of image paths
-    if conf["ds_info"] == 'binary':
-        labeled_ds = list_ds.map(process_path_bin, num_parallel_calls=AUTOTUNE)
-    else:
-        labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-        
-    # Split into train, test and validation data
-    train_size = int(0.7 * ds_size)
-    test_size = int(0.15 * ds_size)
-    val_size = int(0.15 * ds_size)
-    
-    train_ds = labeled_ds.take(train_size)
-    test_ds = labeled_ds.skip(train_size)
-    val_ds = test_ds.skip(val_size)
-    test_ds = test_ds.take(test_size)
-
-    # Print info about the dataset split
-    if verbosity > 0:
-        print ("\n{:32} {:>5}".format("Full dataset sample size:", ds_size))
-        print ("{:32} {:>5}".format("Train dataset sample size:", train_size))
-        print ("{:32} {:>5}".format("Test dataset sample size:", test_size))
-        print ("{:32} {:>5}".format("Validation dataset sample size:", val_size))
-    
-    # Create cache-dir if not already exists
-    pathlib.Path(conf["cache_dir"]).mkdir(parents=True, exist_ok=True)
-    
-    # Cache, shuffle, repeat, batch, prefetch pipeline
-    train_ds = prepare_for_training(train_ds, 'train', conf, cache=True)
-    test_ds = prepare_for_training(test_ds, 'test', conf, cache=True)
-    val_ds = prepare_for_training(val_ds, 'val', conf, cache=True)
-            
-    conf["class_names"] = class_names
-    conf["num_classes"] = num_classes
-    conf["sizes"] = {"total":ds_size, 
-                  "train":train_size, 
-                  "test":test_size, 
-                  "val":val_size}
-    conf["steps"] = {"train":train_size//conf["batch_size"], 
-                  "test":test_size//conf["batch_size"],
-                  "val":val_size//conf["batch_size"]}
-    
-    return train_ds, test_ds, val_ds
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-def create_binary_dataset(conf):
-    """
-    new binary fjkalsdø
-    """
-    global POS_CLASS_NAMES
-    global IMG_SIZE
-    IMG_SIZE = conf["img_shape"][0]
-    # Some parameters
-    data_dir = conf["data_dir"]
-    outcast = conf["outcast"]
-    verbosity = conf["verbosity"]
-
-    pathlib.Path(conf["cache_dir"]).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(conf["log_dir"]).mkdir(parents=True, exist_ok=True)
-    np.random.seed(seed=conf["seed"])
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    ds_size = len(list(data_dir.glob('*/*/*.*g')))
-    directories = np.array(
-        [item.name for item in data_dir.glob('train/*') if item.name != 'metadata.json'])
-    
-    # Remove the outcast folder
-    if outcast != None:
-        directories = np.delete(directories, np.where(outcast == directories))
-        if verbosity > 0: print ("Removed outcast:", outcast, end="\n\n")
-
-    ## Print info about the dataset
-    # Binary dataset
-    class_names = np.array(['Negative','Positive'])
-    conf["num_classes"] = len(class_names)
-    neg_class_name = conf['neg_class'] # 'normal'-class
-    pos_class_names = np.delete(directories, np.where(neg_class_name == directories))
-    POS_CLASS_NAMES = pos_class_names
-    
-    # Print info about neg/pos split
-    ds_size, neg_count, pos_count = print_bin_class_info(
-                                        directories, data_dir, 
-                                        ds_size, outcast, class_names, 
-                                        neg_class_name, pos_class_names
-    )
-    conf["neg_count"] = neg_count
-    conf["pos_count"] = pos_count
-    
-    # Create a tf.dataset of the file paths
-    ds = {split: str(data_dir/split/'*/*.*g') for split in ["train", "test", "val"]}
-    ds_sizes = {name:len(list(glob.glob(path))) for (name, path) in ds.items()}
-    ds_sizes["total"] = ds_size
-    
-    if verbosity > 1:
-        print ("Paths to each split:")
-        [print(key) for key in ds.values()]
-        print ()
-    
-    for split in ds:
-        ds[split] = tf.data.Dataset.list_files(
-                                    ds[split],
-                                    shuffle=conf["shuffle_buffer_size"]>1,
-                                    seed=tf.constant(conf["seed"], tf.int64) if conf["seed"] else None
-                                    )
-    # Create dataset of label, image pairs from the tf.dataset of image paths
-    for split in ds:
-        ds[split] = ds[split].map(process_path_bin, num_parallel_calls=AUTOTUNE)
-    
-    # Save a clean copy of training data
-    clean_train = ds["train"]                                
-    
-    # print info about the dataset split
-    if verbosity:
-        cnt_per_class = get_dataset_info(class_names, data_dir, ds_size)
-        print_split_info(ds, class_names, cnt_per_class, ds_sizes)
-    
-    # Cache, shuffle, repeat, batch, prefetch pipeline
-    for split in ds:
-        ds[split] = prepare_for_training(ds[split], split, conf, cache=True)
-    
-    steps = {name:ds_sizes[name]//conf["batch_size"] for (name, data) in ds.items()}
-    conf["class_names"] = class_names
-    conf["ds_sizes"] = ds_sizes
-    conf["steps"] = steps
-    ds["clean_train"] = clean_train
-    
-    return ds
